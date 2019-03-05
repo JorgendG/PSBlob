3d1eba2d0c7f579c7e95d9fa9716f55e1a99f437
Jorgen de Gier <jwdegier@wxs.nl>
2019-03-05 22:35:43 +0100

    refactor

--- Blob.ps1
+++ Blob.ps1
@@ -1,13 +1,27 @@
 $accountname="salononblob"
-$key = Get-Content .\keys.txt
-$verb = "GET"
+$masterkey = Get-Content .\keys.txt
 
-$RESTAPI_URL = "https://$accountname.blob.core.windows.net/?comp=list"
-$date=(Get-Date).ToUniversalTime()
-$datestr=$date.ToString("R");
-$xmsversion = "2015-02-21"
+# generate authorization key
+Function Generate-MasterKeyAuthorizationSignature
+{
+	[CmdletBinding()]
+	Param
+	(
+		[Parameter(Mandatory=$true)][String]$verb,
+		[Parameter(Mandatory=$true)][String]$accountname,
+        [Parameter(Mandatory=$false)][String]$container,
+        [Parameter(Mandatory=$true)][String]$operation,
+		[Parameter(Mandatory=$true)][String]$dateTime,
+		[Parameter(Mandatory=$true)][String]$key
+	)
 
-$stringtosign = "$verb`n" + # verb
+	$xmsversion = "2015-02-21"
+    if( $container.Length -gt 0 )
+    {
+        $container = "$container/"
+    }
+
+    $stringtosign = "$verb`n" + # verb
                 "`n" +     # content encoding
                 "`n" +     # content language
                 "`n" +     # content length
@@ -19,26 +33,49 @@ $stringtosign = "$verb`n" + # verb
                 "`n" +     # if none match
                 "`n" +     # if unmodified since
                 "`n" +     # range
-                "x-ms-date:$datestr`nx-ms-version:$xmsversion`n" +     # CanonicalizedHeaders
-                "/$accountname/`ncomp:list"       # header 2
+                "x-ms-date:$dateTime`nx-ms-version:$xmsversion`n" +     # CanonicalizedHeaders
+                "/$accountname/$container`n$operation"       # header 2
+    [byte[]]$dataBytes = ([System.Text.Encoding]::UTF8).GetBytes($stringtosign)
+    $hmacsha256 = New-Object System.Security.Cryptography.HMACSHA256
+    $hmacsha256.Key = [Convert]::FromBase64String($key)
+    $sig = [Convert]::ToBase64String($hmacsha256.ComputeHash($dataBytes))
+    $authhdr = "SharedKey $accountname`:$sig"
  
+    $authhdr
+ }
 
-[byte[]]$dataBytes = ([System.Text.Encoding]::UTF8).GetBytes($stringtosign)
-$hmacsha256 = New-Object System.Security.Cryptography.HMACSHA256
-$hmacsha256.Key = [Convert]::FromBase64String($key)
-$sig = [Convert]::ToBase64String($hmacsha256.ComputeHash($dataBytes))
-$authhdr = "SharedKey $accountname`:$sig"
- 
-write-host $authhdr
- 
-$RequestHeader = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
- 
-$RequestHeader.Add("x-ms-version", "$xmsversion")
-$RequestHeader.Add("x-ms-date", $datestr) 
-$RequestHeader.Add("Authorization", $authhdr)
 
-$x = Invoke-RestMethod -Uri $RESTAPI_URL -Method $verb -Headers $RequestHeader -SessionVariable bla #-ContentType application/json
- 
-[xml]$responseXml = $x.Substring($x.IndexOf("<"))
 
-$responseXml
+function Get-BlobContainers($accountname, $MasterKey)
+{
+    $Verb = "GET"
+    $dateTime = [DateTime]::UtcNow.ToString("r")
+    $EndPoint = "https://$accountname.blob.core.windows.net/?comp=list"
+
+    $authHeader = Generate-MasterKeyAuthorizationSignature -verb $Verb -accountname $accountname -dateTime $dateTime -key $MasterKey -operation "comp:list"
+
+	$header = @{authorization=$authHeader;"x-ms-version"="2015-02-21";"x-ms-date"=$dateTime}
+    $result = Invoke-RestMethod -Method $Verb -Uri $EndPoint -Headers $header
+    [xml]$responseXml = $result.Substring($result.IndexOf("<"))
+
+    $responseXml.EnumerationResults.Containers.Container
+}
+
+function Get-BlobInContainer($accountname, $container, $MasterKey)
+{
+    $Verb = "GET"
+    $dateTime = [DateTime]::UtcNow.ToString("r")
+    $EndPoint = "https://$accountname.blob.core.windows.net/$container/?comp=list&restype=container"
+
+    $authHeader = Generate-MasterKeyAuthorizationSignature -verb $Verb -accountname $accountname -dateTime $dateTime -key $MasterKey -container $container -operation "comp:list`nrestype:container"
+	$header = @{authorization=$authHeader;"x-ms-version"="2015-02-21";"x-ms-date"=$dateTime}
+    $result = Invoke-RestMethod -Method $Verb -Uri $EndPoint -Headers $header
+    
+    [xml]$responseXml = $result.Substring($result.IndexOf("<"))
+
+    $responseXml.EnumerationResults.Blobs.Blob
+}
+
+Get-BlobContainers -accountname $accountname -MasterKey $MasterKey
+
+Get-BlobInContainer -accountname $accountname -container "restblob" -MasterKey $MasterKey
